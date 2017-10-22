1308375878da3b33dfe6a9397e9a1f258f10e9a7
andrewvanbeek-okta <andrew.vanbeek@okta.com>
2017-10-22 17:02:28 -0500

    Update okta_configure.js

--- okta_configure.js
+++ okta_configure.js
@@ -1,10 +1,10 @@
-Template.configureLoginServiceDialogForImgur.helpers({
+Template.configureLoginServiceDialogForOkta.helpers({
   siteUrl: function () {
     return Meteor.absoluteUrl();
   }
 });
 
-Template.configureLoginServiceDialogForImgur.fields = function () {
+Template.configureLoginServiceDialogForOkta.fields = function () {
   return [
     {property: 'clientId', label: 'Client Id'},
     {property: 'secret', label: 'Client Secret'}
