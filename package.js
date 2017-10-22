608782904506b2fd1570625a993e02c9f32b490b
andrewvanbeek-okta <andrew.vanbeek@okta.com>
2017-10-22 16:51:57 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'andrewvanbeek-okta:oktaMeteor',
+  name: 'andrewvanbeek-okta:oktameteor',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: '',
